4ef17f72e88a91eba1e7d5c89a638b9f8af2babb
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-18 11:35:07 -0600

    Adding title correctly

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, Head, NextScript } from 'next/document'
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head title='Blog'>
+                {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+                <title key="title">Blog</title>
                 <link rel="shortcut icon" href="/favicon.png" />
             </Head>
             <body>
