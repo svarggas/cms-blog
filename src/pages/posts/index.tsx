115b16577835d27de8dac4f1010843a8d86962b2
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-18 01:03:19 -0600

    Post structure updated

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -13,11 +13,17 @@ const Posts: FC<PostProps> = ({ posts }) => {
     return (
         <section className='section'>
             <div className='container'>
-                <ul>
-                    {posts.map(post => (
-                        <PostCard key={post.fields.slug} post={post} />
-                    ))}
-                </ul>
+                <h4 className="text-4xl font-bold text-gray-800 tracking-widest uppercase text-center">Blog List</h4>
+                <p className="text-center text-gray-600 text-sm mt-2">Here are some of the last blogs posted</p>
+                <section className="text-gray-600 body-font">
+                    <div className="container px-5 py-24 mx-auto">
+                        <div className="flex flex-wrap -m-4">
+                            {posts.map(post => (
+                                <PostCard key={post.fields.slug} post={post} />
+                            ))}
+                        </div>
+                    </div>
+                </section>
             </div>
         </section>
     )
