485df389ebb61fa0f3d75ea5ef2849df37288b8a
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-18 01:02:18 -0600

    Updating card style

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -15,58 +15,32 @@ const PostCard: FC<PostCardProps> = ({ post }) => {
     const { title, slug, excerpt, coverImage, author, date } = post.fields
 
     return (
-        <li className="flex items-center justify-center px-4">
-            <Link href={`/posts/${slug}`} aria-label={title}>
-                <div className="container px-5 mx-auto">
-                    <div className="p-5 bg-white flex items-center mx-auto border-b  mb-10 border-gray-200 rounded-lg sm:flex-row flex-col">
-                        <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10">
-                            <ContentfulImage
-                                alt={`Cover Image for ${title}`}
-                                src={coverImage.fields.file.url}
-                                width={coverImage.fields.file.details.image.width}
-                                height={coverImage.fields.file.details.image.height}
-                                style={{
-                                    width: 'inherit',
-                                    height: 'inherit',
-                                }}
-                            />
-                        </div>
-                        <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                            <h1 className="text-black text-2xl title-font font-bold mb-2">
-                                {title}
-                            </h1>
-                            <p className="leading-relaxed text-base">
-                                {excerpt}
-                            </p>
-                            <div className="py-4">
-                                {/* Tags loop */}
-                                <div className=" inline-block mr-2" >
-                                    <div className="flex  pr-2 h-full items-center">
-                                        <svg className="text-yellow-500 w-6 h-6 mr-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" />
-                                            <circle cx="12" cy="12" r="9" />
-                                            <path d="M9 12l2 2l4 -4" />
-                                        </svg>
-                                        <p className="title-font font-medium">Python</p>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="md:flex font-bold text-gray-800">
-                                <div className="w-full grid grid-cols-2">
-                                    <h2 className="text-gray-500 mr-5">
-                                        By {author.fields.name}
-                                    </h2>
-                                    <p>
-                                        Published: <DateComponent dateString={date} />
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+        <div className="p-4 md:w-1/3">
+            <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                <ContentfulImage
+                    className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                    alt={`Cover Image for ${title}`}
+                    src={coverImage.fields.file.url}
+                    width={coverImage.fields.file.details.image.width}
+                    height={coverImage.fields.file.details.image.height}
+                />
+                <div className="p-6">
+                    <h1 className="title-font text-lg font-medium text-gray-600 mb-3">
+                        {title}
+                    </h1>
+                    <p className="leading-relaxed mb-3">
+                        {excerpt}
+                    </p>
+                    <div className="flex items-center flex-wrap ">
+                        <Link href={`/posts/${slug}`}>
+                            <button className="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md shadow-cla-blue px-4 py-1 rounded-lg">
+                                Learn more
+                            </button>
+                        </Link>
                     </div>
                 </div>
-            </Link>
-        </li>
+            </div>
+        </div>
     )
 }
 
