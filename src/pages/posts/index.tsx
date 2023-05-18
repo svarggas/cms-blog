6b712be0d805689ef3b900fd5b4555c46d4b1fca
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-17 18:35:55 -0600

    Setting a single column on feed

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -13,7 +13,7 @@ const Posts: FC<PostProps> = ({ posts }) => {
     return (
         <section className='section'>
             <div className='container'>
-                <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-10'>
+                <ul>
                     {posts.map(post => (
                         <PostCard key={post.fields.slug} post={post} />
                     ))}
